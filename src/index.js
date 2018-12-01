3de182fdcc52115f86b25f0f269f9a9ec6e21f40
Julian Burgess <aubergene@gmail.com>
2018-12-01 12:09:27 +0000

    Separating out in to two packages

--- src/index.js
+++ src/index.js
@@ -1,2 +1,75 @@
-export * from './hpgl';
-export * from './canvas';
+import { identity, rotate, scale, translate, fromObject, compose, applyToPoint } from 'transformation-matrix';
+
+const round = Math.round
+const ln = ';\n'
+
+export default class CanvasHpgl {
+  constructor() {
+    this.beginPath()
+    this.resetTransform()
+  }
+
+  rotate(a, cx, cy) {
+    this._matrix = compose(this._matrix, rotate(a, cx, cy))
+  }
+  scale(sx, sy) {
+    this._matrix = compose(this._matrix, scale(sx, sy))
+  }
+  translate(tx, ty) {
+    this._matrix = compose(this._matrix, translate(tx, ty))
+  }
+  transform(a, b, c, d, e, f) {
+    this._matrix = compose(this._matrix, fromObject({ a, b, c, d, e, f }))
+  }
+  resetTransform() {
+    this._transformStack = []
+    this._matrix = identity()
+  }
+  save() {
+    this._transformStack.push(toString(this._matrix))
+  }  
+  restore() {
+    if (this._transformStack.length) {
+      this._matrix = fromString(this._transformStack.pop())
+    }
+  }
+
+  beginPath() {
+    this._ = []
+    this._x0 = this._y0 = null; // start of current subpath
+    this._x1 = this._y1 = null; // end of current subpath
+  }
+
+  moveTo(x, y) {
+    y *= -1;
+    ({ x, y } = applyToPoint(this._matrix, { x, y }));
+    this._cmd('PU', round(this._x0 = this._x1 = +x), round(this._y0 = this._y1 = +y))
+  }
+  lineTo(x, y) {
+    y *= -1;
+    ({ x, y } = applyToPoint(this._matrix, { x, y }));
+    this._cmd('PD', round(this._x1 = +x), round(this._y1 = +y));
+  }
+  toString() {
+    if (!this._.length) return ""
+    return this._.map(d => d.join(' ')).join(ln) + ln
+  }
+
+  _cmd(cmd, ...params) {
+    params = params.join(' ')
+    if (this._.length) {
+      const prevCmd = this._[this._.length-1]
+      if (prevCmd[0] === cmd) {
+        if (prevCmd[0] === 'PU') {
+          // Replace previous move commands with the new one
+          // to remove long lists of pointless consecutive moves
+          this._.pop()
+        } else if (prevCmd[1] === params) {
+          // Don't draw to the same point more than once
+          return
+        }
+      }
+    }
+    this._.push([cmd, params])
+  }
+}
--- test/hpgl.js
+++ test/hpgl.js
@@ -1,16 +1,16 @@
 const assert = require('assert')
 const sinon = require('sinon')
-const { Hpgl } = require('../dist/index.umd')
+const CanvasHpgl = require('../dist/index.umd')
 
 describe('Hpgl', function () {
 
   it("hpgl.toString() returns empty string when no commands", function () {
-    var p = new Hpgl();
+    var p = new CanvasHpgl();
     assert.equal(p.toString(), "");
   })
 
   it("hpgl.toString() returns string of commands", function () {
-    var p = new Hpgl();
+    var p = new CanvasHpgl();
     assert.equal(p.toString(), "");
     p.moveTo(150, 50);
     assert.equal(p.toString(), "PU 150 -50;\n");
@@ -23,7 +23,7 @@ describe('Hpgl', function () {
   })
 
   it("hpgl.beginPath() clears the buffer of commands", function () {
-    var p = new Hpgl();
+    var p = new CanvasHpgl();
     assert.equal(p.toString(), "");
     p.moveTo(150, 50);
     p.beginPath()
@@ -31,13 +31,13 @@ describe('Hpgl', function () {
   })
 
   it("hpgl.moveTo(x, y) appends PU commands", function () {
-    var p = new Hpgl();
+    var p = new CanvasHpgl();
     p.moveTo(150, 50);
     assert.equal(p.toString(), "PU 150 -50;\n");
   });
 
   it("hpgl.moveTo(x, y) reduces multiple consecutive PU commands to the last one", function () {
-    var p = new Hpgl();
+    var p = new CanvasHpgl();
     p.moveTo(150, 50);
     assert.equal(p.toString(), "PU 150 -50;\n");
     p.moveTo(200, 100);
@@ -47,7 +47,7 @@ describe('Hpgl', function () {
   });
 
   it("hpgl.moveTo(x, y) reduces multiple consecutive PD commands to same point with a single command", function () {
-    var p = new Hpgl();
+    var p = new CanvasHpgl();
     p.lineTo(150, 50);
     assert.equal(p.toString(), "PD 150 -50;\n");
     p.lineTo(150, 50);
@@ -57,7 +57,7 @@ describe('Hpgl', function () {
   });
 
   it("hpgl.lineTo(x, y) appends PD commands", function () {
-    var p = new Hpgl();
+    var p = new CanvasHpgl();
     p.lineTo(150, 50);
     assert.equal(p.toString(), "PD 150 -50;\n");
     p.lineTo(200, 100);
@@ -67,7 +67,7 @@ describe('Hpgl', function () {
   });
 
   it("hpgl.scale(x, y) scales commands", function () {
-    var p = new Hpgl();
+    var p = new CanvasHpgl();
     p.moveTo(0, 0);
     p.lineTo(100, 0);
     assert.equal(p.toString(), "PU 0 0;\nPD 100 0;\n");
@@ -78,7 +78,7 @@ describe('Hpgl', function () {
   });
 
   it("hpgl.resetTransform() removes the transforms commands", function () {
-    var p = new Hpgl();
+    var p = new CanvasHpgl();
     p.moveTo(0, 0);
     p.scale(2);
     p.lineTo(100, 0);
