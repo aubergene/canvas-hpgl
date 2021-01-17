646d127759006a0ede831d459f57294023a0e489
Julian Burgess <aubergene@gmail.com>
2021-01-17 11:07:03 +0000

    update dependencies
    Fixes #1 beziers should be good now, also fixes problem with closePath

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,37 @@
-import buble from 'rollup-plugin-buble';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import buble from "@rollup/plugin-buble";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 
-export default [{
-  input: 'src/index.js',
-  external: ['adaptive-bezier-curve', 'transformation-matrix'],
-  plugins: [
-    buble()
-  ],
-  output: {
-    file: 'dist/canvas-hpgl.es.js',
-    format: 'es',
-    sourcemap: true
-  }
-}, {
-  input: 'src/index.js',
-  plugins: [
-    buble(),
-    resolve(),
-    commonjs()
-  ],
-  output:
+export default [
   {
-    file: 'dist/canvas-hpgl.umd.js',
-    name: 'CanvasHpgl',
-    format: 'umd',
-    sourcemap: true
-  }
-}, {
-  input: 'src/index.js',
-  plugins: [
-    buble(),
-    resolve(),
-    commonjs(),
-    terser()
-  ],
-  output:
+    input: "src/index.js",
+    external: ["adaptive-bezier-curve", "transformation-matrix"],
+    plugins: [buble()],
+    output: {
+      file: "dist/canvas-hpgl.es.js",
+      format: "es",
+      sourcemap: true,
+    },
+  },
   {
-    file: 'dist/canvas-hpgl.umd.min.js',
-    name: 'CanvasHpgl',
-    format: 'umd',
-    sourcemap: true
-  }
-}]
+    input: "src/index.js",
+    plugins: [buble(), resolve(), commonjs()],
+    output: {
+      file: "dist/canvas-hpgl.umd.js",
+      name: "CanvasHpgl",
+      format: "umd",
+      sourcemap: true,
+    },
+  },
+  {
+    input: "src/index.js",
+    plugins: [buble(), resolve(), commonjs(), terser()],
+    output: {
+      file: "dist/canvas-hpgl.umd.min.js",
+      name: "CanvasHpgl",
+      format: "umd",
+      sourcemap: true,
+    },
+  },
+];
